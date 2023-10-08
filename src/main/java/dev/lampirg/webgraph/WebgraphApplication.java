29a0c6233436ed3c8dd2633bb8fac1766717e238
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-10-08 14:05:27 +0300

    Create WebgraphApplication.java

---
+++ src/main/java/dev/lampirg/webgraph/WebgraphApplication.java
@@ -0,0 +1,13 @@
+package dev.lampirg.webgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebgraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebgraphApplication.class, args);
+	}
+
+}
