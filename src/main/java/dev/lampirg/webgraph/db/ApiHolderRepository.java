7934739b7bdd4fbed7ce8b213aecad79c5a89fee
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-10-18 18:11:52 +0300

    Create ApiHolderRepository.java

---
+++ src/main/java/dev/lampirg/webgraph/db/ApiHolderRepository.java
@@ -0,0 +1,12 @@
+package dev.lampirg.webgraph.db;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import reactor.core.publisher.Mono;
+
+
+public interface ApiHolderRepository extends ReactiveCrudRepository<ApiHolder, String> {
+    Mono<ApiHolder> findByUsername(@NotNull String username);
+
+    Mono<ApiHolder> findByApiKey(@NotNull String apiKey);
+}
