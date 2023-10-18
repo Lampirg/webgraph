9ce7b2f7a91182ad4ecaeed0b21cb261da805123
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-10-18 18:17:53 +0300

    Create MongoTests.java

---
+++ src/test/java/dev/lampirg/webgraph/MongoTests.java
@@ -0,0 +1,148 @@
+package dev.lampirg.webgraph;
+
+import dev.lampirg.webgraph.db.ApiHolder;
+import dev.lampirg.webgraph.db.EntityInformationProvider;
+import dev.lampirg.webgraph.db.ReactiveMongoApiHolderRepository;
+import org.assertj.core.api.Assertions;
+import org.jetbrains.annotations.NotNull;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.context.annotation.Import;
+
+import java.util.List;
+
+@DataMongoTest
+@Import({ReactiveMongoApiHolderRepository.class, EntityInformationProvider.class})
+class MongoTests {
+
+    @Autowired
+    private ReactiveMongoApiHolderRepository apiHolderRepository;
+
+    @BeforeEach
+    void tearDown() {
+        apiHolderRepository.deleteAll().block();
+    }
+
+    @Test
+    void save() {
+        ApiHolder expected = ApiHolder.user("user", "key");
+        apiHolderRepository.save(expected).block();
+        ApiHolder actual = apiHolderRepository.findByUsername(expected.getUsername()).block();
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void saveAll() {
+        List<ApiHolder> expected = createThreeUsers();
+        apiHolderRepository.saveAll(expected).blockLast();
+        List<ApiHolder> actual = apiHolderRepository.findAll().collectList().block();
+        Assertions.assertThat(actual).hasSameElementsAs(expected);
+    }
+
+    @Test
+    void findByKey() {
+        ApiHolder expected = ApiHolder.user("user", "key");
+        apiHolderRepository.save(expected).block();
+        ApiHolder actual = apiHolderRepository.findByApiKey(expected.getApiKey()).block();
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void findAllById() {
+        List<ApiHolder> toSave = createThreeUsers();
+        apiHolderRepository.saveAll(toSave).blockLast();
+        List<String> usernames = List.of("user", "abuser");
+        List<ApiHolder> expected = toSave.stream()
+                .filter(apiHolder -> usernames.contains(apiHolder.getUsername()))
+                .toList();
+        List<ApiHolder> actual = apiHolderRepository.findAllById(usernames).collectList().block();
+        Assertions.assertThat(actual).hasSameElementsAs(expected);
+    }
+
+    @Test
+    void existsById() {
+        List<ApiHolder> toSave = createThreeUsers();
+        apiHolderRepository.saveAll(toSave).blockLast();
+        List<String> usernames = List.of("user", "notuser");
+        boolean actual = apiHolderRepository.existsById(usernames.get(0)).block();
+        Assertions.assertThat(actual).isTrue();
+        actual = apiHolderRepository.existsById(usernames.get(1)).block();
+        Assertions.assertThat(actual).isFalse();
+    }
+
+    @Test
+    void count() {
+        List<ApiHolder> toSave = createThreeUsers();
+        apiHolderRepository.saveAll(toSave).blockLast();
+        long expected = 3;
+        long actual = apiHolderRepository.count().block();
+        Assertions.assertThat(actual).isEqualTo(expected);
+        apiHolderRepository.deleteAll().block();
+        expected = 0;
+        actual = apiHolderRepository.count().block();
+        Assertions.assertThat(actual).isEqualTo(expected);
+    }
+
+    @Test
+    void deleteById() {
+        List<ApiHolder> toSave = createThreeUsers();
+        apiHolderRepository.saveAll(toSave).blockLast();
+        String username = "user";
+        List<ApiHolder> expected = toSave.stream()
+                .filter(apiHolder -> !username.equals(apiHolder.getUsername()))
+                .toList();
+        apiHolderRepository.deleteById(username).block();
+        List<ApiHolder> actual = apiHolderRepository.findAll().collectList().block();
+        Assertions.assertThat(actual).hasSameElementsAs(expected);
+    }
+
+    @Test
+    void deleteAllById() {
+        List<ApiHolder> toSave = createThreeUsers();
+        apiHolderRepository.saveAll(toSave).blockLast();
+        List<String> usernames = List.of("user", "abuser");
+        List<ApiHolder> expected = toSave.stream()
+                .filter(apiHolder -> !usernames.contains(apiHolder.getUsername()))
+                .toList();
+        apiHolderRepository.deleteAllById(usernames).block();
+        List<ApiHolder> actual = apiHolderRepository.findAll().collectList().block();
+        Assertions.assertThat(actual).hasSameElementsAs(expected);
+    }
+
+    @Test
+    void delete() {
+        List<ApiHolder> toSave = createThreeUsers();
+        apiHolderRepository.saveAll(toSave).blockLast();
+        ApiHolder user = toSave.get(0);
+        List<ApiHolder> expected = toSave.stream()
+                .filter(apiHolder -> !user.equals(apiHolder))
+                .toList();
+        apiHolderRepository.delete(user).block();
+        List<ApiHolder> actual = apiHolderRepository.findAll().collectList().block();
+        Assertions.assertThat(actual).hasSameElementsAs(expected);
+    }
+
+    @Test
+    void deleteAll() {
+        List<ApiHolder> toSave = createThreeUsers();
+        apiHolderRepository.saveAll(toSave).blockLast();
+        List<ApiHolder> users = toSave.subList(0, 2);
+        List<ApiHolder> expected = toSave.stream()
+                .filter(apiHolder -> !users.contains(apiHolder))
+                .toList();
+        apiHolderRepository.deleteAll(users).block();
+        List<ApiHolder> actual = apiHolderRepository.findAll().collectList().block();
+        Assertions.assertThat(actual).hasSameElementsAs(expected);
+    }
+
+    @NotNull
+    private List<ApiHolder> createThreeUsers() {
+        return List.of(
+                ApiHolder.user("user", "key"),
+                ApiHolder.user("abuser", "kokoko"),
+                ApiHolder.user("hello", "world")
+        );
+    }
+}
