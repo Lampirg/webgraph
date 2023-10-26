563397db2869619f64c2c8764b0f4ddea5b4aaf1
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-10-26 15:30:17 +0300

    Create SecurityConfig.java

--- src/main/java/dev/lampirg/webgraph/security/SecurityConfig.java
+++ src/main/java/dev/lampirg/webgraph/security/SecurityConfig.java
@@ -1,5 +1,7 @@
 package dev.lampirg.webgraph.security;
 
+import dev.lampirg.webgraph.consume.ApiKeyService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.ReactiveAuthenticationManager;
@@ -8,41 +10,36 @@ import org.springframework.security.config.annotation.web.reactive.EnableWebFlux
 import org.springframework.security.config.web.server.SecurityWebFiltersOrder;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 import org.springframework.security.web.server.authentication.AuthenticationWebFilter;
 import org.springframework.security.web.server.authentication.ServerAuthenticationConverter;
 import reactor.core.publisher.Mono;
 
-import java.util.List;
-import java.util.Optional;
-
 @Configuration
 @EnableWebFluxSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final ApiKeyService apiKeyService;
 
     public ReactiveAuthenticationManager reactiveAuthenticationManager() {
-        return authentication -> {
-            if (!authentication.getCredentials().equals("aba")) {
-                authentication.setAuthenticated(false);
-                return Mono.just(authentication);
-            }
-            return Mono.just(authentication);
-        };
+        return Mono::just;
     }
 
     public ServerAuthenticationConverter serverAuthenticationConverter() {
-        return exchange -> {
-            Optional<String> key = Optional.ofNullable(exchange.getRequest().getHeaders().getFirst("Key"));
-            Authentication authentication =
-                    new UsernamePasswordAuthenticationToken(
-                            "anon",
-                            key.orElse(""),
-                            List.of(new SimpleGrantedAuthority("ROLE_USER"))
-                    );
-            return Mono.just(authentication);
-        };
+        return exchange -> Mono.justOrEmpty(exchange.getRequest().getHeaders().getFirst("Key"))
+                .flatMap(apiKeyService::findByApiKey)
+                .map(apiHolder -> UsernamePasswordAuthenticationToken.authenticated(
+                        apiHolder.getUsername(), apiHolder.getApiKey(), apiHolder.getAuthorities()
+                ))
+                .cast(Authentication.class)
+                .switchIfEmpty(Mono.just(notGranted()));
+    }
+
+    public Authentication notGranted() {
+        return UsernamePasswordAuthenticationToken.unauthenticated(
+                "not_granted",
+                "");
     }
 
 
@@ -57,7 +54,9 @@ public class SecurityConfig {
     public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
         http
                 .authorizeExchange(exchangeSpec -> exchangeSpec
+                        .pathMatchers("/key/**").hasRole("ADMIN")
                         .anyExchange().authenticated())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .addFilterAt(filter(), SecurityWebFiltersOrder.AUTHENTICATION);
         return http.build();
     }
--- src/test/java/dev/lampirg/webgraph/integration/StarWarsInfoControllerSecurityTests.java
+++ src/test/java/dev/lampirg/webgraph/integration/StarWarsInfoControllerSecurityTests.java
@@ -1,6 +1,8 @@
 package dev.lampirg.webgraph.integration;
 
+import dev.lampirg.webgraph.consume.ApiKeyService;
 import dev.lampirg.webgraph.consume.ResidentSearcher;
+import dev.lampirg.webgraph.db.ApiHolder;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
@@ -9,6 +11,7 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 import java.util.NoSuchElementException;
 
@@ -20,6 +23,9 @@ class StarWarsInfoControllerSecurityTests {
     @MockBean
     private ResidentSearcher residentSearcher;
 
+    @MockBean
+    private ApiKeyService apiKeyService;
+
     @BeforeEach
     void setUp(ApplicationContext context) {
         testClient = WebTestClient.bindToApplicationContext(context).build();
@@ -27,6 +33,7 @@ class StarWarsInfoControllerSecurityTests {
 
     @Test
     void givenExistentName() {
+        Mockito.when(apiKeyService.findByApiKey("aba")).thenReturn(Mono.just(ApiHolder.user("User", "aba")));
         Mockito.when(residentSearcher.findResidentsFromSamePlanet("Luke Skywalker"))
                 .thenReturn(Flux.just("C-3PO", "Darth Vader"));
         String expected = """
@@ -47,6 +54,7 @@ class StarWarsInfoControllerSecurityTests {
 
     @Test
     void givenNonExistentName() {
+        Mockito.when(apiKeyService.findByApiKey("aba")).thenReturn(Mono.just(ApiHolder.user("User", "aba")));
         Mockito.when(residentSearcher.findResidentsFromSamePlanet("Luke Skywalker"))
                 .thenThrow(new NoSuchElementException());
         testClient.get()
@@ -66,6 +74,7 @@ class StarWarsInfoControllerSecurityTests {
 
     @Test
     void givenWrongAuthorization() {
+        Mockito.when(apiKeyService.findByApiKey("daba")).thenReturn(Mono.empty());
         testClient.get()
                 .uri("/info/same-residents?name=Luke+Skywalker")
                 .header("Key", "daba")
