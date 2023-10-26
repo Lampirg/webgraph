c26c3f7bd884ba8051e7ad41b466b799211c726c
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-10-26 15:28:59 +0300

    Create HardcodedAdmin.java

---
+++ src/main/java/dev/lampirg/webgraph/consume/HardcodedAdmin.java
@@ -0,0 +1,33 @@
+package dev.lampirg.webgraph.consume;
+
+import dev.lampirg.webgraph.db.ApiHolder;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import reactor.core.publisher.Mono;
+
+// TODO: replace hardcoded admin
+
+@Configuration
+@Profile("hardcodedAdmin")
+@RequiredArgsConstructor
+public class HardcodedAdmin {
+
+    private final ApiKeyService apiKeyService;
+
+    @Bean
+    public CommandLineRunner createAdmin() {
+        return args -> {
+            String apiKey = "ADMINHRDC";
+            apiKeyService.containsKey(apiKey)
+                    .flatMap(aBoolean -> Boolean.FALSE.equals(aBoolean) ?
+                            apiKeyService.save(ApiHolder.admin("Hardcoded Admin User", apiKey)) :
+                            Mono.empty()
+                    )
+                    .block();
+        };
+    }
+
+}
