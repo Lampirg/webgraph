ec028b47a108c190d768a2ad737ef43cf550c849
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-10-27 14:58:35 +0300

    Create Basic pages
    A bit trashy commit(
    My first frontend app thought.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,19 +1,15 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
+import HeaderComponent from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+    <img alt="Vue logo" class="logo" src="./assets/space-station.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+      <HeaderComponent />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
     </div>
   </header>
 
@@ -34,7 +30,7 @@ header {
 nav {
   width: 100%;
   font-size: 12px;
-  text-align: center;
+  text-align: right;
   margin-top: 2rem;
 }
 
---
+++ frontend/src/components/GetResidents.vue
@@ -0,0 +1,17 @@
+<script setup>
+
+import StarWarsService from "../service/StarWarsService";
+
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+    <li v-for="(resident, index) in StarWarsService.get(name)" :key="index">
+        {{ resident.name }}
+    </li>
+</template>
