a9cbd73ceffea0e61a09bc4eb09dd0f2f8fbc949
Lampirg <63813365+Lampirg@users.noreply.github.com>
2023-10-28 19:10:29 +0300

    Fix: incorrect security handling
    Admin API key was forbidden when accessing user API.

--- backend/src/test/java/dev/lampirg/webgraph/integration/StarWarsInfoControllerSecurityTests.java
+++ backend/src/test/java/dev/lampirg/webgraph/integration/StarWarsInfoControllerSecurityTests.java
@@ -3,12 +3,14 @@ package dev.lampirg.webgraph.integration;
 import dev.lampirg.webgraph.service.apikey.ApiKeyService;
 import dev.lampirg.webgraph.service.resident.ResidentSearcher;
 import dev.lampirg.webgraph.db.ApiHolder;
+import org.jetbrains.annotations.NotNull;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
@@ -16,6 +18,7 @@ import reactor.core.publisher.Mono;
 import java.util.NoSuchElementException;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class StarWarsInfoControllerSecurityTests {
 
     private WebTestClient testClient;
@@ -36,14 +39,7 @@ class StarWarsInfoControllerSecurityTests {
         Mockito.when(apiKeyService.findByApiKey("aba")).thenReturn(Mono.just(ApiHolder.user("User", "aba")));
         Mockito.when(residentSearcher.findResidentsFromSamePlanet("Luke Skywalker"))
                 .thenReturn(Flux.just("C-3PO", "Darth Vader"));
-        String expected = """
-                {
-                    data: [
-                        {"name": "C-3PO"},
-                        {"name": "Darth Vader"}
-                    ]
-                }
-                """;
+        String expected = getTypicalExpectedOutput();
         testClient.get()
                 .uri("/info/same-residents?name=Luke+Skywalker")
                 .header("Key", "aba")
@@ -80,5 +76,31 @@ class StarWarsInfoControllerSecurityTests {
                 .header("Key", "daba")
                 .exchange()
                 .expectStatus().isForbidden();
+    }
+
+    @Test
+    void givenAdmin() {
+        Mockito.when(apiKeyService.findByApiKey("aba")).thenReturn(Mono.just(ApiHolder.admin("Admin", "aba")));
+        Mockito.when(residentSearcher.findResidentsFromSamePlanet("Luke Skywalker"))
+                .thenReturn(Flux.just("C-3PO", "Darth Vader"));
+        String expected = getTypicalExpectedOutput();
+        testClient.get()
+                .uri("/info/same-residents?name=Luke+Skywalker")
+                .header("Key", "aba")
+                .exchange()
+                .expectStatus().is2xxSuccessful()
+                .expectBody().json(expected);
+    }
+
+    @NotNull
+    private static String getTypicalExpectedOutput() {
+        return """
+                {
+                    data: [
+                        {"name": "C-3PO"},
+                        {"name": "Darth Vader"}
+                    ]
+                }
+                """;
     }
 }
